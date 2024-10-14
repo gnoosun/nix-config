d161abb84ab59a049692203c9d5347ab829f812c
gnoosun <gnooroo@gmail.com>
2024-10-13 20:24:27 -0700

    boot: limit boot entries to 5

--- hosts/framework13_amd7040/configuration.nix
+++ hosts/framework13_amd7040/configuration.nix
@@ -12,6 +12,7 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 1;
 
