3ef12f06356d4fedec732b4859d755b4aa81e191
gnoosun <gnooroo@gmail.com>
2024-10-13 22:33:28 -0700

    helper: simplify helper a bit

--- hosts/framework13_amd7040/install.sh
+++ hosts/framework13_amd7040/install.sh
@@ -5,12 +5,8 @@ sudo mv /etc/nixos/*.nix /tmp/
 sudo ln -sf "$ABSPATH"/configuration.nix /etc/nixos/configuration.nix
 sudo ln -sf "$ABSPATH"/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 
-[ ! -f ~/.config/i3/config ] || mv ~/.config/i3/config /tmp/i3_config
-[ ! -f ~/.config/i3status/config ] || mv ~/.config/i3status/config /tmp/i3status_config
+[ ! -d ~/.config/i3 ] || mv ~/.config/i3 /tmp/
+ln -sf "$ABSPATH"/.config/i3 ~/.config/i3
 
-[ ! -d ~/.config/i3 ] || mkdir -p ~/.config/i3
-[ ! -d ~/.config/i3status ] || mkdir -p ~/.config/i3status
-ln -sf "$ABSPATH"/.config/i3/config ~/.config/i3/config
-ln -sf "$ABSPATH"/.config/i3status/config ~/.config/i3status/config
-
-sudo nixos-rebuild switch
+[ ! -d ~/.config/i3status ] || mv ~/.config/i3status /tmp/
+ln -sf "$ABSPATH"/.config/i3status ~/.config/i3status
