3de2cbf67b779c23dfedd7f5b2a8e38c15fd6af8
gnoosun <gnooroo@gmail.com>
2024-10-15 20:59:20 -0700

    nixos: add support for bluetooth

--- hosts/framework13_amd7040/hardware-configuration.nix
+++ hosts/framework13_amd7040/hardware-configuration.nix
@@ -35,4 +35,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 }
