23ea8072d2ccde7a97b0cb8fdd1f9e8351ec9119
gnoosun <gnooroo@gmail.com>
2024-10-19 13:08:36 -0700

    enable flakes

--- hosts/framework13_amd7040/configuration.nix
+++ hosts/framework13_amd7040/configuration.nix
@@ -16,6 +16,8 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 1;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
