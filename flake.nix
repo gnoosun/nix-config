3eb0662e6c538198f371c7025b518f635a258435
gnoosun <gnooroo@gmail.com>
2025-06-07 22:59:32 -0700

    switch to flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,11 @@
 {
   description = "A very basic flake";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
-
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    nixosConfigurations.nixos= nixpkgs.lib.nixosSystem {
+      modules = [ ./configuration.nix ];
+    };
   };
 }
