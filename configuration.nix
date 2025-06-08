2ca18522ea97c5dc7e6613436166aa7d80b2ef2f
gnoosun <gnooroo@gmail.com>
2025-06-07 23:57:39 -0700

    remove flake, system is now busted

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 1;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [ "nix-command" ];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
